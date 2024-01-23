645c3558354a03fff0c996c1a2292e5952e482b0
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-01-23 11:35:02 +0300

    updated additem adapter

--- app/src/main/java/com/example/adminwaveoffood/adapter/AddItemAdpater.kt
+++ app/src/main/java/com/example/adminwaveoffood/adapter/AddItemAdpater.kt
@@ -1,34 +1,89 @@
 package com.example.adminwaveoffood.adapter
 
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.adminwaveoffood.databinding.ItemItemBinding
 
-class AddItemAdapter(private val MenuItemName:ArrayList<String>, private val MenuItemPrice: ArrayList<String>, private val MenuItemImage: ArrayList<Int>): RecyclerView.Adapter<AddItemAdapter.AddItemViewHolder>() {
+class AddItemAdapter(
+    private val menuItemName: ArrayList<String>,
+    private val menuItemPrice: ArrayList<String>,
+    private val menuItemImage: ArrayList<Int>
+) : RecyclerView.Adapter<AddItemAdapter.AddItemViewHolder>() {
 
+    private val itemQuantities = IntArray(menuItemName.size)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddItemViewHolder {
-         val binding = ItemItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = ItemItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return AddItemViewHolder(binding)
     }
 
-
-
     override fun onBindViewHolder(holder: AddItemViewHolder, position: Int) {
-         holder.bind(position)
+        holder.bind(position)
     }
-    override fun getItemCount(): Int = MenuItemName.size
-    inner class AddItemViewHolder(private val binding:  ItemItemBinding) :RecyclerView.ViewHolder(binding.root){
+
+    override fun getItemCount(): Int = menuItemName.size
+
+    inner class AddItemViewHolder(private val binding: ItemItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
         fun bind(position: Int) {
-             binding.apply {
-                 foodNameTextView.text=MenuItemName[position]
-                 priceTextView.text=MenuItemPrice[position]
-                 foodImageView.setImageResource(MenuItemImage[position])
-             }
+            binding.apply {
+                val quantity = itemQuantities[position]
+                foodNameTextView.text = menuItemName[position]
+                priceTextView.text = menuItemPrice[position]
+                foodImageView.setImageResource(menuItemImage[position])
+quantityTextView.text = quantity.toString()
+
+                minusButton.setOnClickListener {
+                    decreaseQuantity(position)
+                }
+                plusButton.setOnClickListener {
+                    increaseQuantity(position)
+                }
+                deleteButton.setOnClickListener {
+                    deleteQuantity(position)
+                }
+            }
+        }
+
+        private fun increaseQuantity(position: Int) {
+            if (itemQuantities[position] < 10) {
+                itemQuantities[position]++
+                binding.quantityTextView.text=itemQuantities[position].toString()
+            }
+        }
+
+        private fun decreaseQuantity(position: Int) {
+            if (itemQuantities[position]>1){
+                itemQuantities[position]--
+                binding.quantityTextView.text=itemQuantities[position].toString()
+            }
+        }
+
+        private fun deleteQuantity(position: Int) {
+            menuItemName.removeAt(position)
+            menuItemPrice.removeAt(position)
+            menuItemImage.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position,menuItemName.size)
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
